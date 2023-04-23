a1cd142b5efe9e3142bf00108871a2deebe1b113
yair-levy <yair.levy@namogoo.com>
2023-04-23 23:28:32 +0300

    gh pages build

--- quasar.config.js
+++ quasar.config.js
@@ -57,7 +57,7 @@ module.exports = configure(function (/* ctx */) {
         node: 'node16'
       },
 
-      vueRouterMode: 'hash' // available values: 'hash', 'history'
+      vueRouterMode: 'hash', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
@@ -71,7 +71,7 @@ module.exports = configure(function (/* ctx */) {
       // ignorePublicFolder: true,
       // minify: false,
       // polyfillModulePreload: true,
-      // distDir
+      distDir:'docs'
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
